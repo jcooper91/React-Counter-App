5ad5df2d7b5815ab4cfb2eac2aa5b84556460d55
Jack Cooper <JCooper@executivegrapevine.com>
2018-12-07 16:09:43 +0000

    added props to counter 1:50:45

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,18 +4,27 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 0, value: 4 },
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
-      { id: 3, value: 0 }
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
     ]
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
   };
 
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} />
+          <Counter
+            onDelete={this.handleDelete}
+            key={counter.id}
+            counter={counter}
+          />
         ))}
       </div>
     );
